a846245e05728b183993ed23c6ada5d5a0706d44
maryembrik <maryembrik@users.noreply.github.com>
2024-09-19 21:49:03 +0100

    https://github.com/maryembrik/Java_Tds.git

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
 
-        int nbcages;
+      /*  int nbcages;
         String zooName;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the number of the zoo ");
         zooName = sc.nextLine();
         System.out.println("Please enter the name of  cages");
         nbcages = sc.nextInt();
-        System.out.println(zooName+" has "+nbcages+" cages");
-        System.out.println(MyZoo.toString());
-    }
-
-    public class Animal {
+        System.out.println(zooName+" has "+nbcages+" cages");*/
         Animal lion = new Animal("lion", "sauvage", 10, true);
+        Zoo myZoo = new Zoo("frigya", "sousse", 20);
+
+        myZoo.displayZoo();
+        System.out.println(myZoo);
+        System.out.println(lion);
+    }
+}
+    class Animal {
+        //Animal lion = new Animal("lion", "sauvage", 10, true);
         String name;
         String family;
         int age;
@@ -29,25 +34,30 @@ public class Main {
             this.isMammal = isMammal;
 
         }
-public class Zoo {
-    Zoo myZoo = new Zoo("frigya", "sousse", 20);
+        public String toString() {
+            return "Animal Name: " + name + ", Family: " + family + ", Age: " + age + ", Is Mammal: " + isMammal;
+        }
+    }
+ class Zoo {
+    //Zoo myZoo = new Zoo("frigya", "sousse", 20);
+    Animal[] animals = new Animal[25];
     String nameZ;
     String City;
     int nbrCages;
 
     public  Zoo(String nameZ, String City, int nbrCages) {
-        Animal[] animals = new Animal[nbrCages];
         this.nameZ = nameZ;
         this.City = City;
         this.nbrCages = nbrCages;
 
 
     }
-    public void MyZoo() {
-        System.out.println("Myzoo "+nameZ+ "in"+City+"have"+nbrCages+" cages");
+    public void displayZoo() {
+        System.out.println("Zoo Name: " + nameZ);
+        System.out.println("City: " + City);
+        System.out.println("Number of Cages: " + nbrCages);
 
     }
 }
 
-    }
-}
+
